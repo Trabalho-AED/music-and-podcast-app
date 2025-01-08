d81bddaf552495ea37915a276afdf0b5aede7b1f
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-01-08 01:25:51 +0000

    Register and Login logic almost-finished, awaiting UI to continue. CamelCase in use for variables, and snake_case for functions. Comments to most code added.

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light" Alterar entre tema escuro e claro
 #customtkinter.set_default_color_theme(".\\theme\\rime.json") # Tema de cores
 
-accounts_path = "user_accounts.csv" # Caminho para o ficheiro onde são armazenadas as contas
-admin_listfile = "admin_list.csv" # Caminho para o ficheiro onde são armazenadas as contas que são admin
+accountsPath = "user_accounts.csv" # Caminho para o ficheiro onde são armazenadas as contas
+adminListfile = "admin_list.csv" # Caminho para o ficheiro onde são armazenadas as contas que são admin
 
 # Inicializar app
 app = customtkinter.CTk()
 
 # Titulo da app
-app.title("Login Form")
+app.title("Music App")
 
 # Define a dimensão da app
 appWidth = 600
@@ -26,125 +26,219 @@ y = (screenHeight / 2) - (appHeight / 2)
 # Define o tamanho da app e começa no centro da tela
 app.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
 
+
+
+##################[ALGORITMOS DA APP]################################
+def read_file(path):
+    """Lê um ficheiro com qualquer opção.
+    Retorna o conteúdo do ficheiro"""
+
+    #Abre o ficheiro
+    with open(path, "r", encoding="utf-8") as file:
+        lines = file.readlines() # Lê o ficheiro
+    
+    return lines # Retorna o conteúdo
+
 def get_accounts(username,password):
-    with open(accounts_path, encoding="utf-8") as file:
-        lines = file.readlines()
+    """Verifica se existe o username ou a combinação username - password.
+    Retorna os dados encontrados"""
+
+    lines = read_file(accountsPath) # Recebe o conteúdo do ficheiro
     
+    #Para cada linha
     for line in lines:
-        campos = line.strip().split(";")
-        if campos[0] == username and campos[1] == password:
-            return campos[0], campos[1], campos[2]
+        fields = line.strip().split(";")
+        if fields[0] == username and fields[1] == password:
+            return fields[0], fields[1], fields[2] # Se a combinação username e password existir, retorna todos os dados
     
-    return "not_found", "not_found", ""
+    return "not_found", "not_found", "" # Caso a combinação não exista, retorna as strings
+
+def user_check(username):
+    """Verifica se o username já existe.
+    Retorna um booleano"""
+
+    lines = read_file(accountsPath) # Abrir os dados do ficheiro utilizador
+
+    #Para cada linha de dados
+    for line in lines:
+        fields = line.strip().split(";")
+        if fields[0] == username:
+            return True # Se o nome de utilizador(fields na posição 0) for igual ao username
+    
+    return False # Se não for encontrado nenhum username igual
 
 def check_admin(username):
-    with open(admin_listfile, encoding="utf-8") as file:
-        lines = file.readlines()
+    """Verifica se o utilizador é admin.
+    Retorna um booleano"""
 
+    lines = read_file(adminListfile) # Abrir os dados do ficheiro admin
+
+    #Para cada linha de dados
     for line in lines:
         if line.strip() == username:
-            return True
-    return False
+            return True # Se o utilizador na linha for igual ao username
+        
+    return False # Se o utilizador não estiver na lista de admin
 
-# Criar o formulário de login
-def login_action(username_entry, password_entry, result_label):
-    username = username_entry.get()
-    password = password_entry.get()
+def create_account(username, password, name):
+    """Cria uma conta"""
+
+    accountAdd = username+";"+password+";"+name+"\n" # String com o Formato dos dados
+    with open(accountsPath, "a", encoding="utf-8") as file:
+        file.write(accountAdd) # Escreve os dados no ficheiro
+
+def login_action(usernameEntry, passwordEntry, resultLabel,loginFrame):
+    """Gere o algoritmo de login"""
+    username = usernameEntry.get() # Recebe o valor que está na entry do username
+    password = passwordEntry.get() # Recebe o valor que está na entry da password
+
+    # Se algum campo estiver vazio
+    if username == "" or password == "": 
+        resultLabel.configure(text="Preencha todos os campos.") # Texto a apresentar
+        return 
+    
+    print(f"Username: {username}, Password: {password}")  # Mensagem de confirmação na consola
+
+    username, password, name = get_accounts(username, password) # Verifica se a combinação utilizador/password existe
+
+    isAdmin = check_admin(username) # Booleano - Verifica se o utilizador é admin
+    if isAdmin: # Se for admin, atribui a flag admin ao utilizador
+        adminflag = "Admin"
+    else: # Se não for admin, atribui a flag não admin ao utilizador
+        adminflag = "Não Admin"
+
+    if username == "not_found" and password == "not_found": # Se o utilizador e a password não existirem ou a password estiver errada
+        resultLabel.configure(text="Utilizador Inexistente ou password errada.") # Texto a apresentar
+
+    else: # Se a combinação utilizador password estiver correta 
+        resultLabel.configure(text=f"Bem vindo {name}, Login realizado com sucesso!\nTipo de Utilizador: {adminflag}") # Texto a apresentar
+        print(username, password, adminflag) # Confirmação
+        mainwindow_render(loginFrame) # Passa para a janela principal
+
+def register_action(usernameEntry, passwordEntry,nameEntry, resultLabel):
+    """Gere o algoritmo de registo"""
+
+    name = nameEntry.get() # Recebe o valor que está na entry do nome
+    username = usernameEntry.get() # Recebe o valor que está na entry do username
+    password = passwordEntry.get() # Recebe o valor que está na entry da password
+
+    # Se algum campo estiver vazio
     if username == "" or password == "":
-        result_label.configure(text="Preencha todos os campos.")
+        resultLabel.configure(text="Preencha todos os campos.") # Texto a apresentar
         return
     print(f"Username: {username}, Password: {password}")  # Substituir por lógica real de autenticação
-    username, password, name = get_accounts(username, password)
 
-    is_admin = check_admin(username)
-    if is_admin:
-        adminflag = "Admin"
+    isUser = user_check(username) # Booleano - Verifica se o utilizador já existe
+
+    # Caso o utilizador já exista
+    if isUser:
+        resultLabel.configure(text="Utilizador já existe!.") # Texto a apresentar
+    #Caso o utilizador não exista
     else:
-        adminflag = "Não Admin"
-    if username == "not_found" and password == "not_found":
-        result_label.configure(text="Utilizador Inexistente.")
-    else:
-        result_label.configure(text=f"Bem vindo {name}, Login realizado com sucesso!\nTipo de Utilizador: {adminflag}")
+        create_account(username, password, name) # Criar a conta
+        resultLabel.configure(text=f"Bem vindo {name}, Conta criada com com sucesso!") # Texto a apresentar
+
+##########################################################
 
 
-################[RENDER SCREENS]##########################
+
+
+#############################[RENDER SCREENS]##########################################
 def register_render(oldFrame):
-    print("Register")
-    oldFrame.pack_forget()
-    ################[LOGIN FORM]##############################
+    """Renderiza a frame do formulário de registo"""
+
+    print("Register") # Mensagem de confirmação na consola
+
+    oldFrame.pack_forget() # Apagar o estilo do frame anterior
+
     #Frame
     frameRegister = customtkinter.CTkFrame(app, width=600, height=500)
     frameRegister.pack(expand=True)
 
     # Labels e campos de entrada
-    name_label = customtkinter.CTkLabel(frameRegister, text="Nome:")
-    name_label.pack(padx=20, pady=5)
+    nameLabel = customtkinter.CTkLabel(frameRegister, text="Nome:")
+    nameLabel.pack(padx=20, pady=5)
 
-    name_entry = customtkinter.CTkEntry(frameRegister, placeholder_text="Nome...")
-    name_entry.pack(padx=20, pady=10)
+    nameEntry = customtkinter.CTkEntry(frameRegister, placeholder_text="Nome...")
+    nameEntry.pack(padx=20, pady=10)
 
-    username_label = customtkinter.CTkLabel(frameRegister, text="Username:")
-    username_label.pack(padx=20, pady=5)
+    usernameLabel = customtkinter.CTkLabel(frameRegister, text="Username:")
+    usernameLabel.pack(padx=20, pady=5)
 
-    username_entry = customtkinter.CTkEntry(frameRegister, placeholder_text="Username...")
-    username_entry.pack(padx=20, pady=10)
+    usernameEntry = customtkinter.CTkEntry(frameRegister, placeholder_text="Username...")
+    usernameEntry.pack(padx=20, pady=10)
 
-    password_label = customtkinter.CTkLabel(frameRegister, text="Password:")
-    password_label.pack(padx=20, pady=5)
+    passwordLabel = customtkinter.CTkLabel(frameRegister, text="Password:")
+    passwordLabel.pack(padx=20, pady=5)
 
-    password_entry = customtkinter.CTkEntry(frameRegister, placeholder_text="Password...", show="*")
-    password_entry.pack(padx=20, pady=10)
+    passwordEntry = customtkinter.CTkEntry(frameRegister, placeholder_text="Password...", show="*")
+    passwordEntry.pack(padx=20, pady=10)
 
     # Botão de login
-    login_button = customtkinter.CTkButton(frameRegister, text="Login", command=lambda:login_render(frameRegister))
-    login_button.pack(padx=20, pady=5)
+    loginButton = customtkinter.CTkButton(frameRegister, text="Login", command=lambda:login_render(frameRegister))
+    loginButton.pack(padx=20, pady=5)
 
     # Botão de criar conta
-    login_button = customtkinter.CTkButton(frameRegister, text="Criar Conta", command=register_render)
-    login_button.pack(padx=20, pady=5)
+    loginButton = customtkinter.CTkButton(frameRegister, text="Criar Conta", command=lambda:register_action(usernameEntry, passwordEntry,nameEntry, resultLabel))
+    loginButton.pack(padx=20, pady=5)
 
     # Label para exibir resultados ou mensagens de erro
-    result_label = customtkinter.CTkLabel(frameRegister, text="")
-    result_label.pack(padx=20, pady=20)
-    #######################################################
+    resultLabel = customtkinter.CTkLabel(frameRegister, text="")
+    resultLabel.pack(padx=20, pady=20)
 
-##########################################################
 
 def login_render(oldFrame):
+    """Renderiza a frame do formulário de login"""
+
+    #Caso não exista frame anterior (ex: Ao executar a app)
     if oldFrame == "":
         pass
     else:
-        oldFrame.pack_forget()
-    ################[LOGIN FORM]##############################
+        oldFrame.pack_forget() # Apagar o estilo do frame anterior
+
     #Frame
     frameLogin = customtkinter.CTkFrame(app, width=600, height=500)
     frameLogin.pack(expand=True)
 
     # Labels e campos de entrada
-    username_label = customtkinter.CTkLabel(frameLogin, text="Username:")
-    username_label.pack(padx=20, pady=5)
+    usernameLabel = customtkinter.CTkLabel(frameLogin, text="Username:")
+    usernameLabel.pack(padx=20, pady=5)
 
-    username_entry = customtkinter.CTkEntry(frameLogin, placeholder_text="Username...")
-    username_entry.pack(padx=20, pady=10)
+    usernameEntry = customtkinter.CTkEntry(frameLogin, placeholder_text="Username...")
+    usernameEntry.pack(padx=20, pady=10)
 
-    password_label = customtkinter.CTkLabel(frameLogin, text="Password:")
-    password_label.pack(padx=20, pady=5)
+    passwordLabel = customtkinter.CTkLabel(frameLogin, text="Password:")
+    passwordLabel.pack(padx=20, pady=5)
 
-    password_entry = customtkinter.CTkEntry(frameLogin, placeholder_text="Password...", show="*")
-    password_entry.pack(padx=20, pady=10)
+    passwordEntry = customtkinter.CTkEntry(frameLogin, placeholder_text="Password...", show="*")
+    passwordEntry.pack(padx=20, pady=10)
 
     # Botão de login
-    login_button = customtkinter.CTkButton(frameLogin, text="Login", command=lambda:login_action(username_entry, password_entry, result_label))
-    login_button.pack(padx=20, pady=5)
+    loginButton = customtkinter.CTkButton(frameLogin, text="Login", command=lambda:login_action(usernameEntry, passwordEntry, resultLabel, frameLogin))
+    loginButton.pack(padx=20, pady=5)
 
     # Botão de criar conta
-    login_button = customtkinter.CTkButton(frameLogin, text="Criar Conta", command=lambda:register_render(frameLogin))
-    login_button.pack(padx=20, pady=5)
+    createaccButton = customtkinter.CTkButton(frameLogin, text="Criar Conta", command=lambda:register_render(frameLogin))
+    createaccButton.pack(padx=20, pady=5)
 
     # Label para exibir resultados ou mensagens de erro
-    result_label = customtkinter.CTkLabel(frameLogin, text="")
-    result_label.pack(padx=20, pady=20)
-    #######################################################
+    resultLabel = customtkinter.CTkLabel(frameLogin, text="")
+    resultLabel.pack(padx=20, pady=20)
+
+
+def mainwindow_render(oldFrame):
+    """Rendriza a frame da janela principal"""
+
+    oldFrame.pack_forget() # Apagar o estilo do frame anterior
+
+    frameMain = customtkinter.CTkFrame(app, width=600, height=500)
+    frameMain.pack(expand=True)
+    
+    sometextLabel = customtkinter.CTkLabel(frameMain, text="You are in the main window!")
+    sometextLabel.pack(padx=20, pady=5)
+
+##########################################################
+
 
 login_render("")
 # Loop de event listening
